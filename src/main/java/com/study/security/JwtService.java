b1cd5ae70c77c4ec001ede059eef0e87f425d801
Inrikys <inrikys@gmail.com>
2022-05-18 21:09:15 -0300

    feat: jwt initial structure

---
+++ src/main/java/com/study/security/JwtService.java
@@ -0,0 +1,60 @@
+package com.study.security;
+
+import com.study.security.model.UserSale;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.expiration}")
+    private String expiration;
+
+    @Value("${security.jwt.key}")
+    private String key;
+
+    public String getToken(UserSale userSale) {
+        long exp = Long.valueOf(expiration);
+        LocalDateTime dateTimeExp = LocalDateTime.now().plusMinutes(exp);
+        Date dateExp = Date.from(dateTimeExp.atZone(ZoneId.systemDefault()).toInstant());
+
+        return Jwts.builder()
+                .setSubject(userSale.getLogin())
+                .setExpiration(dateExp)
+                .signWith(SignatureAlgorithm.HS512, key)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Claims claims = getClaims(token);
+            Date dateExpiration = claims.getExpiration();
+            LocalDateTime localDateTimeExp = dateExpiration.toInstant()
+                    .atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+            return LocalDateTime.now().isAfter(localDateTimeExp);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUserSalesLogin(String token) {
+        return (String) getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token) throws ExpiredJwtException {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
